135816bb8ec1c8faa69cb249dc5f609db5a05a1d
Josh Cantie <jcantie3@gmail.com>
2024-10-25 20:48:17 -0400

    created login page on front-end and created back-end route for logging in

--- back-end/server.js
+++ back-end/server.js
@@ -39,6 +39,38 @@ app.post("/register", async (req, res) => {
     }
 });
 
+app.post("/login", async (req, res) => {
+    const { username, password } = req.body;
+    if (!username || !password) {
+        return res.sendStatus(400);
+    }
+
+    try {
+        const [rows] = await db.pool.execute(
+            "SELECT * FROM users WHERE username = ?",
+            [username]
+        );
+        const user = rows[0];
+
+        if (!user) {
+            return res.sendStatus(404);
+        }
+
+        const isPasswordValid = await bcrypt.compare(password, user.password);
+        if (!isPasswordValid) {
+            return res.sendStatus(401);
+        }
+
+        res.json({
+            id: user.user_id,
+            username: user.username,
+        });
+    } catch (err) {
+        console.error(err);
+        res.sendStatus(500);
+    }
+});
+
 app.listen(PORT, () => {
     console.log(`Example app is listening on port ${PORT}.`);
 });
--- front-end/src/pages/LoginPage.vue
+++ front-end/src/pages/LoginPage.vue
@@ -1,3 +1,47 @@
 <template>
-    <div>Hello</div>
+    <div>
+        <AppNavBar />
+        <div
+            class="bg-electric-blue min-h-screen flex flex-col items-center justify-center"
+        >
+            <div class="text-white text-6xl mb-10">Login</div>
+
+            <div class="flex flex-col w-1/4 space-y-4">
+                <div class="flex items-center">
+                    <label for="username" class="text-white text-lg w-1/3"
+                        >Username</label
+                    >
+                    <input
+                        v-model="username"
+                        id="username"
+                        type="text"
+                        placeholder="Enter your username"
+                        class="px-4 py-2 rounded-lg w-2/3 text-lg"
+                    />
+                </div>
+
+                <div class="flex items-center">
+                    <label for="password" class="text-white text-lg w-1/3"
+                        >Password</label
+                    >
+                    <input
+                        v-model="password"
+                        id="password"
+                        type="password"
+                        placeholder="Enter your password"
+                        class="px-4 py-2 rounded-lg w-2/3 text-lg"
+                    />
+                </div>
+            </div>
+
+            <div class="mt-6">
+                <AppButton label="Login" @click="handleSignUp" />
+            </div>
+        </div>
+    </div>
 </template>
+
+<script setup>
+import AppNavBar from "../components/AppNavBar.vue";
+import AppButton from "../components/AppButton.vue";
+</script>
